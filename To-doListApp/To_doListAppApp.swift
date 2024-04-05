d2fa7d0d25358532719d4909b277dd505c4760b9
Ansheng Zhou <azhou@lcs.on.ca>
2024-04-05 09:46:42 -0400

    Initial Commit

---
+++ To-doListApp/To_doListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_doListAppApp.swift
+//  To-doListApp
+//
+//  Created by Ansheng Zhou on 2024-04-05.
+//
+
+import SwiftUI
+
+@main
+struct To_doListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
